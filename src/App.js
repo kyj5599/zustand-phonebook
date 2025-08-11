b29295cd5fc1b3ec27729250effd3b5df734a343
yujin <kimyujin.ked@baenergy.net>
2025-08-11 17:20:30 +0900

    ✨ 연락처앱 레이아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Grid from "@mui/material/Grid";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>연락처 앱</h1>
+      <Grid container spacing={2}>
+        <Grid size={6}>
+          <ContactForm />
+        </Grid>
+        <Grid size={6}>
+          <ContactList />
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Box, Button } from "@mui/material";
+
+const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+      <TextField
+        id="name"
+        label="이름"
+        variant="outlined"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        id="phone-number"
+        label="전화번호"
+        variant="outlined"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <Button variant="contained">추가</Button>
+    </Box>
+  );
+};
+
+export default ContactForm;
